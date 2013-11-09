3c62b7dd0797b2f489ed918393484da23e46a59e
Neilson <neilsonlima@gmail.com>
2013-11-09 00:22:38 -0200

    Update layout

--- apps/participantes/views.py
+++ apps/participantes/views.py
@@ -1,10 +1,11 @@
 # coding: utf-8
 
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def cadastrar(request):
 	return HttpResponse(u'Página de cadastro dos Participantes')
 
 def listagem(request):
-    return HttpResponse(u'Página inicial dos Participantes')
+    #return HttpResponse(u'Página inicial dos Participantes')
+    return render(request, 'index.html')
