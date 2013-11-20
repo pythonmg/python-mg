41064ddb1392ffa2022ed3e16464441654efc1d0
Igor dos Santos <igr.exe@gmail.com>
2013-11-19 23:06:47 -0200

    email unique, url null

--- apps/membros/models.py
+++ apps/membros/models.py
@@ -9,8 +9,8 @@ CHOICES_SEXO = (('M', 'Masculino'), ('F', 'Feminino'))
 
 class Membro(models.Model):
     nome = models.CharField(max_length=150)
-    email = models.EmailField(max_length=75)
-    url = models.URLField(max_length=200)
+    email = models.EmailField(max_length=75, unique=True)
+    url = models.URLField(max_length=200, null=True)
     data_nascimento = models.DateField()
     genero = models.CharField(max_length=1, choices=CHOICES_SEXO)
     foto = models.ImageField(upload_to='membros/', default='membros/avatar.gif')
