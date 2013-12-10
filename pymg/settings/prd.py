797a03f73b8d3cc1630856c90c65ce401f0cffab
Lucas Magnum <lucasmagnum2@gmail.com>
2013-12-10 11:19:30 -0200

    Settings para produção.

---
+++ pymg/settings/prd.py
@@ -0,0 +1,11 @@
+from .common import *
+
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+ALLOWED_HOSTS = ['pythonmg.com.br']
+
+
+STATIC_ROOT = '/home/lucasmagnum/webapps/pythonmg_static/'
+STATIC_URL = '/static/'
