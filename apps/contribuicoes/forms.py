58b803cda4bdb6b0d6c76af8d6855dfeab15cc32
Lucas Simon <lucassrod@gmail.com>
2014-04-16 18:45:24 -0300

    Cria o form de contribuicoes
    Criado o form baseado no ModelForm do django e no model Contribuicoes

--- apps/contribuicoes/forms.py
+++ apps/contribuicoes/forms.py
@@ -3,7 +3,16 @@
 # Stdlib imports
 
 # Core Django imports
+from django.forms import ModelForm
 
 # Third-party app imports
 
 # Imports from your apps
+from .models import Contribuicoes
+
+
+class ContribuicaoForm(ModelForm):
+    """Cria um form baseado no model de contribuicoes"""
+    class Meta:
+        model = Contribuicoes
+        fields = ['titulo', 'descricao']
