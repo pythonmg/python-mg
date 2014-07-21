87aafabf985761f75c87f1d9a4cd1954523875cf
igor santos <igr.exe@gmail.com>
2014-07-21 16:29:34 -0300

    add default secret_key

--- pymg/settings/common.py
+++ pymg/settings/common.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).ancestor(3)
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default='123456')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
