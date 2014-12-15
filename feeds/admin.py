f41e22fa7f0090e4cad316eb552263e7c3323d8c
Lucas Magnum <lucas.oliveira@titansgroup.com.br>
2014-12-15 19:06:20 -0200

    feeds pep8

--- feeds/admin.py
+++ feeds/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from feeds.models import Feed, FeedItem
-# Register your models here.
+
 admin.site.register(Feed)
 admin.site.register(FeedItem)
-
--- feeds/management/commands/update_feeds.py
+++ feeds/management/commands/update_feeds.py
@@ -1,11 +1,15 @@
-from django.core.management.base import BaseCommand, CommandError
-from feeds.models import Feed, FeedItem
-import feedparser
 import pprint
-pp = pprint.PrettyPrinter(indent=4)
+import feedparser
+
 from time import mktime
 from datetime import datetime
+
+from django.core.management.base import BaseCommand
 from django.utils.html import strip_tags
+
+from feeds.models import Feed, FeedItem
+
+pp = pprint.PrettyPrinter(indent=4)
 
 
 class Command(BaseCommand):
@@ -14,9 +18,11 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         feeds = Feed.objects.all()
         n_feed_items = 0
+
         for feed in feeds:
             print feed.title
             parsed = feedparser.parse(feed.feed_url)
+
             for entry in parsed.entries:
                 print entry.title
                 pp.pprint(entry)
@@ -32,7 +38,7 @@ class Command(BaseCommand):
                         link=entry.link,
                         summary=strip_tags(entry.summary),
                         guid=guid,
-                        )
+                    )
 
                     #parse meetup, github and google groups
                     if entry.link.startswith('http://www.meetup.com'):
@@ -47,18 +53,13 @@ class Command(BaseCommand):
                             dt = datetime.fromtimestamp(mktime(entry['updated_parsed']))
                             feed_item.date_modified = dt
 
-
                     if entry.link.startswith('https://groups.google.com'):
                         feed_item.author = entry.author
                         if 'published_parsed' in entry:
                             dt = datetime.fromtimestamp(mktime(entry['published_parsed']))
                             feed_item.date_modified = dt
 
-                    # print 'before saving'
-                    # print feed_item, feed_item.date_modified
                     feed_item.save()
-                    n_feed_items    += 1
-                # poll.opened = False
-                # poll.save()
+                    n_feed_items += 1
 
         self.stdout.write('Successfully imported "%s" feed items ' % n_feed_items)
